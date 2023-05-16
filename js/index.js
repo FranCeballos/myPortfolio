195e3d0c7c83aa9b90253e423140a9411d253c8d
Francisco Ceballos <ceballosfranciscomartin@gmail.com>
2023-05-16 17:50:42 -0300

    About Section UI
    - Also some style changes to hero and navbar

--- js/index.js
+++ js/index.js
@@ -1,14 +1,1 @@
 "use-strict";
-
-/* const toggleSwitch = document.querySelector("#theme-toggle");
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
-}
-
-toggleSwitch.addEventListener("change", switchTheme, false);
- */
